3d5ab950c55f730939188b2ab8ba62dad80e19da
Naresh0591 <110534928+Naresh0591@users.noreply.github.com>
2026-04-05 08:56:55 +0530

    Update navbar.js

--- hotstar/src/components/navbar.js
+++ hotstar/src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
+import SearchBar from "./search-bar";
 import "./Navbar.css";
 
 function Navbar({ onSearch }) {
